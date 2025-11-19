ecef81040f0efe83c2b76e90b6528f07f813c770
Deep Akbari <ackberry@Deeps-MacBook-Air.local>
2025-11-19 15:18:12 -0500

    half baked driver class

---
+++ build/CMakeFiles/ride_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ride_server.dir/src/location.cpp.o"
+  "CMakeFiles/ride_server.dir/src/location.cpp.o.d"
+  "CMakeFiles/ride_server.dir/src/main.cpp.o"
+  "CMakeFiles/ride_server.dir/src/main.cpp.o.d"
+  "ride_server"
+  "ride_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ride_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ride_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ride_server.
+# This may be replaced when dependencies are built.
